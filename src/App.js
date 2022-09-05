fc898da78e6a00b8ebf741a6d22094e441aaca5a
Drishti Saraf <106866070+drishti1920@users.noreply.github.com>
2022-09-05 18:46:16 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./styles.css";
+import Quote from "./components/Quote.js";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Quote />
+    </div>
+  );
+}
